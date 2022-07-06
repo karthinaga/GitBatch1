1fe8ae048fadac9038bb8223a56966d0bc1a2965
karthinaga <arkarthick171@gmail.com>
2022-07-06 21:23:50 +0530

    c3

--- src/org/com/Sam.java
+++ src/org/com/Sam.java
@@ -1,6 +1,10 @@
 package org.com;
 
 public class Sam {
+	private void m1() {
+		System.out.println("rocket");
+
+	}
 	private void m3() {
 		System.out.println("PSLV C53");
 
@@ -8,6 +12,7 @@ public class Sam {
 	public static void main(String[] args) {
 		Sam s = new Sam();
 		s.m3();
+		s.m1();
 	}
 	   
 	   
