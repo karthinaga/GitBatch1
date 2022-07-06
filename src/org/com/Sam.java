055797ee9474cc11d0ed61c513fac73bc0281fe5
karthinaga <arkarthick171@gmail.com>
2022-07-06 12:40:38 +0530

    my code is done

--- src/org/com/Sam.java
+++ src/org/com/Sam.java
@@ -1,13 +1,22 @@
 package org.com;
 
 public class Sam {
+	private void m1() {
+		System.out.println("i am");
+
+	}
 	private void m3() {
 		System.out.println("PSLV C53");
 
 	}
+	private void mark3() {
+		System.out.println("c53");
+	}
 	public static void main(String[] args) {
 		Sam s = new Sam();
 		s.m3();
+		s.mark3();
+		s.m1();
 	}
 	   
 	   
