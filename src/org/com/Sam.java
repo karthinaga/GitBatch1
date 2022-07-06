89b8a0f5e8ffd512058f00a79230791ccd5eeda6
karthinaga <arkarthick171@gmail.com>
2022-07-06 11:13:23 +0530

    master work is done

---
+++ src/org/com/Sam.java
@@ -0,0 +1,16 @@
+package org.com;
+
+public class Sam {
+	private void m3() {
+		System.out.println("PSLV C53");
+
+	}
+	public static void main(String[] args) {
+		Sam s = new Sam();
+		s.m3();
+	}
+	   
+	   
+
+	}
+
